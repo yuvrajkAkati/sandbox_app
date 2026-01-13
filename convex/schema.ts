5fbdd7f012a3d1421b10524d886b277655d2c188
yuvrajkAkati <139755961+yuvrajkAkati@users.noreply.github.com>
2026-01-13 15:37:00 +0530

    backend

---
+++ convex/schema.ts
@@ -0,0 +1,13 @@
+import { defineSchema,defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+    documents : defineTable({
+        title : v.string(),
+        content : v.string(),
+        createdAt : v.number(),
+        updatedAt : v.number()
+    })
+    .index("by_updatedAt",["updatedAt"])
+    .index("by_createdAt",["createdAt"])
+})
